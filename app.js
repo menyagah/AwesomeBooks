dbc648ea6fb2b686cfcc477c915e2b4ce4b5c23d
menyagah <menyagah27@gmail.com>
2021-09-13 22:34:58 +0300

    Add delete function

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const inputData = document.querySelector('.input-data');
 
 let data = [];
 
+const displayBooks = () => {
+    const div = document.createElement('div')
+    div.innerHTML= `<div>title: ${inputTitle.value}</div>
+                    <div>author: ${inputAuthor.value}</div><br>
+                    <button class='remove-btn'>Remove</button>
+                    <hr>`;
+    inputData.appendChild(div)
+    inputAuthor.value = '';
+    inputTitle.value = '';
+}
+
+
+
 addBtn.addEventListener('click', ()=>{
     const id = Math.random().toString(36).substr(0,5);
     data.push({title:inputTitle.value, author:inputAuthor.value, id:id});
@@ -12,13 +25,7 @@ addBtn.addEventListener('click', ()=>{
     displayBooks()
 });
 
-const displayBooks = () => {
-    const div = document.createElement('div')
-    div.innerHTML= `<div>title: ${inputTitle.value}</div>
-                    <div>author: ${inputAuthor.value}</div><br>
-                    <button >Remove</button>
-                    <hr>`
-    inputData.appendChild(div)
-    inputAuthor.value = '';
-    inputTitle.value = '';
-} 
+
+inputData.addEventListener('click', ()=> {
+    console.log('deleted');
+});
