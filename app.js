85dec50e19b67eaed173e9ec0f0bc51941114c24
menyagah <menyagah27@gmail.com>
2021-09-13 20:59:58 +0300

    Create a displaybooks function and add an add button event listener

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const  inputAuthor  = document.querySelector('.input-author');
 const  inputTitle = document.querySelector('.input-title');
 const  addBtn = document.querySelector('.add-btn');
 const inputData = document.querySelector('.input-data');
+
+let data = [];
+
+addBtn.addEventListener('click', ()=>{
+    data.push({title:inputTitle.value, author:inputAuthor.value});
+    displayBooks();
+});
+
+const displayBooks = () => {
+    const div = document.createElement('div')
+    div.innerHTML= `<div>title: ${inputTitle.value}</div><br>
+                    <div>author: ${inputAuthor.value}</div><br>`
+    inputData.appendChild(div)
+    inputAuthor.value = '';
+    inputTitle.value = '';
+} 
