74eab271a9f5f1ae5804c8d0a4cb4ef84374b5bc
menyagah <menyagah27@gmail.com>
2021-09-13 21:07:13 +0300

    Add horizontal line

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ let data = [];
 addBtn.addEventListener('click', ()=>{
     const id = Math.random().toString(36).substr(0,5);
     data.push({title:inputTitle.value, author:inputAuthor.value, id:id});
+    console.log(data);
     displayBooks()
 });
 
 const displayBooks = () => {
     const div = document.createElement('div')
-    div.innerHTML= `<div>title: ${inputTitle.value}</div><br>
-                    <div>author: ${inputAuthor.value}</div><br>`
+    div.innerHTML= `<div>title: ${inputTitle.value}</div>
+                    <div>author: ${inputAuthor.value}</div><br>
+                    <button >Remove</button>
+                    <hr>`
     inputData.appendChild(div)
     inputAuthor.value = '';
     inputTitle.value = '';
