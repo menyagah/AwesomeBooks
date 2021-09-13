110d769d7c5b190ce069896dec278599c48f819d
menyagah <menyagah27@gmail.com>
2021-09-13 23:33:19 +0300

    Add delete function and local storage

--- app.js
+++ app.js
@@ -3,29 +3,42 @@ const  inputTitle = document.querySelector('.input-title');
 const  addBtn = document.querySelector('.add-btn');
 const inputData = document.querySelector('.input-data');
 
-let data = [];
-
 const displayBooks = () => {
-    const div = document.createElement('div')
-    div.innerHTML= `<div>title: ${inputTitle.value}</div>
-                    <div>author: ${inputAuthor.value}</div><br>
-                    <button class='remove-btn'>Remove</button>
+    inputData.innerHTML = '';
+    localStorage.getItem('book');
+    const bookItem = JSON.parse(localStorage.getItem('book') || '[]');
+    bookItem.forEach(({title, author, id }) => {
+        const div = document.createElement('div');
+        div.innerHTML= `<div>title: ${title}</div>
+                    <div>author: ${author}</div><br>
+                    <button class='remove-btn' id='${id}'>Remove</button>
                     <hr>`;
-    inputData.appendChild(div)
-    inputAuthor.value = '';
-    inputTitle.value = '';
+        inputData.appendChild(div)
+        
+    });
 }
 
-
+const deleteBook = (el) => {
+    const books = JSON.parse(localStorage.getItem('book'))
+    localStorage.setItem('book', JSON.stringify(books));
+    const { id } = el;
+    localStorage.setItem('book', JSON.stringify(books.filter((book)=> book.id !== id)))
+    displayBooks()
+}
 
 addBtn.addEventListener('click', ()=>{
+    const books = JSON.parse(localStorage.getItem('book') || '[]');
     const id = Math.random().toString(36).substr(0,5);
-    data.push({title:inputTitle.value, author:inputAuthor.value, id:id});
-    console.log(data);
+    books.push({title:inputTitle.value, author:inputAuthor.value, id:id});
+    localStorage.setItem('book', JSON.stringify(books))
     displayBooks()
+    inputAuthor.value = '';
+    inputTitle.value = '';
 });
 
 
-inputData.addEventListener('click', ()=> {
-    console.log('deleted');
+inputData.addEventListener('click', (e)=> {
+    deleteBook(e.target)
 });
+
+displayBooks()
